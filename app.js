b9b313aa4775629ea9ce00569d6f2e91ebe47478
max-programming <maxprogramming05@gmail.com>
2021-04-06 14:23:37 +0530

    Final JS product

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-// require ./client.js
-import express from "express";
-import fs from "fs";
-import qrcode from "qrcode-terminal";
-import { client, setSessionCfg } from "./client";
+// @ts-check
+const express = require("express");
+const fs = require("fs");
+const { Client } = require("whatsapp-web.js");
+const qrcode = require("qrcode-terminal");
 const app = express();
 
 app.use(express.json());
+
+const SESSION_FILE_PATH = "./session.json";
+let sessionCfg;
+if (fs.existsSync(SESSION_FILE_PATH)) {
+  sessionCfg = require(SESSION_FILE_PATH);
+}
+
+const client = new Client({
+  puppeteer: { headless: false },
+  session: sessionCfg,
+});
 
 client.initialize();
 
@@ -15,11 +26,9 @@ client.on("qr", qr => {
 });
 
 client.on("authenticated", session => {
-  console.log("AUTHENTICATED");
-
-  setSessionCfg(session);
-
-  fs.writeFile("../session.json", JSON.stringify(session), err => {
+  console.log("AUTHENTICATED", session);
+  sessionCfg = session;
+  fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
     if (err) {
       console.error(err);
     }
@@ -36,6 +45,10 @@ client.on("auth_failure", msg => {
 client.on("ready", () => {
   console.log("Client is ready!");
 
+  app.get("/", (req, res) => {
+    res.send("Hiii!");
+  });
+
   app.post("/send-msg", (req, res) => {
     const { number, msg } = req.body;
     client.sendMessage(`91${number}@c.us`, msg).then(() => res.send(msg));
@@ -43,12 +56,8 @@ client.on("ready", () => {
 
   app.get("/logout", async (req, res) => {
     await client.logout();
-
-    fs.writeFile(`../session.json`, "{}", function (err) {
-      console.log(`Error ${err}`);
-    });
-
-    res.send("User logged out");
+    fs.unlink(SESSION_FILE_PATH, () => console.log("Session deleted"));
+    process.exit();
   });
 
   app.listen(3000, () => console.log("Listening on port 3000"));
