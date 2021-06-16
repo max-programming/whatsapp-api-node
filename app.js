2b04b7dac1a8bf1ebdd2511a1be90414c7c06316
Max Programming <ui.sabuwala@gmail.com>
2021-06-16 14:30:52 +0530

    🩹 Added limit file size of 16 mb

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const { Client, MessageMedia } = require("whatsapp-web.js");
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: "16mb" }));
 
 const SESSION_FILE_PATH = "./session.json";
 let sessionCfg;
